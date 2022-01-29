263f1f1cf70ac1f2b98ecc59ae880ca810cd56c4
anh <trinhphuonganh99@gmail.com>
2022-01-29 13:58:17 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 
 
-import React from 'react'
-
+import React, {Fragment} from 'react'
+import 'src/App.css';
 
 let Header = (props) =>  {
 
     return(
-        <h1>g</h1>
+        <Fragment>
+            <div className="header">
+                <div className="logo">
+                    Woof
+                </div>
+                <div className="home-and-signup">
+                    {/* <a href="Home.js" className="home">
+                        <span>Home</span>
+                    </a> */}
+                    
+                </div>
+            </div>
+        </Fragment>
     )
 }
 

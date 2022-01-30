d37337ead50d4e562798c3f7966dcc3eb5845c39
anh <trinhphuonganh99@gmail.com>
2022-01-29 22:28:26 -0500

    Update Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,52 @@
-
-import React, {Fragment} from 'react'
-
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Typography } from '@mui/material';
+import { ReactComponent as YourSvg } from '../static/bg.svg';
 
 let Home = (props) =>  {
-
     return(
-        <Fragment>
-            <div className="row">
-                <div className="welcomeMessage">
-                    <div className="welcome">Welcome to Woof</div>
-                </div>
+        <div id='HOME'>
+            {/* <YourSvg className="homeBackground"/>     */}
+            <div className='homeTitleSection'>
+                <div className='homeTitle'>
+                    Woof
+                </div>      
+                <div className='homeTitleImageSectionContainer'>
+                    <div className='homeTitleImageContainer'>
+                        <div className='homeTitleImage'></div>
+                    </div>                     
+                </div>                
             </div>
-        </Fragment>
-    )
-};
 
+            <div className='homeAboutSection'>
+                <div className='homeAboutImageSectionContainer'>
+                    <div className='homeAboutImageContainer'>
+                        <div className='homeAboutImage'></div>
+                    </div>                     
+                </div>   
+                <div className='homeAbout'>
+                    <Typography
+                        variant="h1"
+                        color="red"
+                    >
+                        About
+                    </Typography>
+                </div>                     
+            </div>
+
+            <div className='homeHowToSection'>
+                <div className='homeHowTo'>
+                    How To
+                </div>      
+                <div className='homeHowToImageSectionContainer'>
+                    <div className='homeHowToImageContainer'>
+                        <div className='homeHowToImage'></div>
+                    </div>                     
+                </div>                
+            </div>
+
+        </div>
+
+    )
+}
 export default (Home);
